552f514d4e682b500887392ccdbc85bbfa16948d
西森大介 <moriwm77@gmail.com>
2017-05-10 15:22:47 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,10 +25,11 @@ namespace :deploy do
   end
 
   task :db_create do
-  on roles(:db) do |host|
-    with rails_env: fetch(:rails_env) do
-      within current_path do
-        execute :bundle, :exec, :rake, 'db:create'
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
       end
     end
   end
