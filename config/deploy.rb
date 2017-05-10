7efb9ecd47a499b03103cf51fb9e3533a1e3cb91
西森大介 <moriwm77@gmail.com>
2017-05-10 15:46:31 +0900

    modify

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,6 @@ set :repo_url, 'git@github.com:mori5321/lyricmaker.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-set :deploy_to, '/var/www/lyricmaker'
-
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
@@ -18,19 +16,8 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 
 after 'deploy:publishing', 'deploy:restart'
-
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
-  end
-
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
-        end
-      end
-    end
   end
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,41 +1,41 @@
-#ワーカーの数。後述
-  $worker  = 2
-#何秒経過すればワーカーを削除するのかを決める
-  $timeout = 30
-#自分のアプリケーション名、currentがつくことに注意。
-  $app_dir = "/var/www/lyricmaker/current"
-#リクエストを受け取るポート番号を指定。後述
-  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-#PIDの管理ファイルディレクトリ
-  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-#エラーログを吐き出すファイルのディレクトリ
-  $std_log = File.expand_path 'log/unicorn.log', $app_dir
+app_path = File.expand_path('../../../', __FILE__)
 
-# 上記で設定したものが適応されるよう定義
-  worker_processes  $worker
-  working_directory $app_dir
-  stderr_path $std_log
-  stdout_path $std_log
-  timeout $timeout
-  listen  $listen
-  pid $pid
+worker_processes 1
 
-#ホットデプロイをするかしないかを設定
-  preload_app true
+working_directory app_path
+pid "#{app_path}/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 
-#fork前に行うことを定義。後述
-  before_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-    old_pid = "#{server.config[:pid]}.oldbin"
-    if old_pid != server.pid
-      begin
-        Process.kill "QUIT", File.read(old_pid).to_i
-      rescue Errno::ENOENT, Errno::ESRCH
-      end
+listen 3000
+timeout 60
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+run_once = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+
+  if run_once
+    run_once = false # prevent from firing again
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
     end
   end
+end
 
-#fork後に行うことを定義。後述
-  after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-  end
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end
